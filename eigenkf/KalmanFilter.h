f368b27f42a70610834c436a6318b09925888aa6
Ryan Pavlik <rpavlik@iastate.edu>
2010-12-08 16:25:36 -0600

    stub in the files

---
+++ eigenkf/KalmanFilter.h
@@ -0,0 +1,20 @@
+/** @file KalmanFilter.h
+	@brief
+
+	@date 2010
+
+	@author
+	Ryan Pavlik
+	<rpavlik@iastate.edu> and <abiryan@ryand.net>
+	http://academic.cleardefinition.com/
+	Iowa State University Virtual Reality Applications Center
+	Human-Computer Interaction Graduate Program
+*/
+
+
+#pragma once
+#ifndef INCLUDED_KalmanFilter_h_GUID_75AD5E97_70AC_4563_A322_C2A627FD07EB
+#define INCLUDED_KalmanFilter_h_GUID_75AD5E97_70AC_4563_A322_C2A627FD07EB
+
+#endif // INCLUDED_KalmanFilter_h_GUID_75AD5E97_70AC_4563_A322_C2A627FD07EB
+
---
+++ kftest.cpp
@@ -0,0 +1,27 @@
+/**
+	@file kftest.cpp
+	@brief
+
+	@date 2010
+
+	@author
+	Ryan Pavlik
+	<rpavlik@iastate.edu> and <abiryan@ryand.net>
+	http://academic.cleardefinition.com/
+	Iowa State University Virtual Reality Applications Center
+	Human-Computer Interaction Graduate Program
+*/
+
+// Internal Includes
+// - none
+
+// Library/third-party includes
+// - none
+
+// Standard includes
+// - none
+
+int main(int argc, char * argv[]) {
+	return 0;
+
+}
