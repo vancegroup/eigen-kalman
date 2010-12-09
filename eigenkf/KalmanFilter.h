9800d1778d4ad63ad686f1b317a5dcbcaea06914
Ryan Pavlik <rpavlik@iastate.edu>
2010-12-09 07:52:32 -0600

    it runs

--- eigenkf/KalmanFilter.h
+++ eigenkf/KalmanFilter.h
@@ -17,6 +17,9 @@
 #define INCLUDED_KalmanFilter_h_GUID_75AD5E97_70AC_4563_A322_C2A627FD07EB
 
 #include <Eigen/Eigen>
+#include <Eigen/Cholesky>
+
+#include <stdexcept>
 
 template<unsigned int dimension>
 class SimpleState {
@@ -81,14 +84,38 @@ class AbsoluteMeasurement {
 public:
 	static const int DIM = StateType::DIM;
 	typedef Eigen::Matrix<double, DIM, 1> Vector;
+	typedef Eigen::Matrix<double, DIM, DIM> Matrix;
 	Vector x;
+	
+	Matrix jacobian;
+	Matrix covariance;
+	
+	AbsoluteMeasurement() :
+		x(Vector::Zero()),
+		jacobian(Matrix::Identity()),
+		covariance(Matrix::Identity()) {}
+	
+	
+	Matrix const& getJacobian(StateType const& state) {
+		return jacobian;
+	}
+	
+	/// Measurement noise covariance, aka uncertainty
+	/// in measurement
+	Matrix const& getCovariance(StateType const& state) {
+		return covariance;
+	}
+	
+	Vector const getInnovation(StateType const& state) {
+		return x - state.vec;
+	}
 	
 };
 
 template<class StateType, class ProcessModelType>
 class KalmanFilter {
 	public:
-	typedef Eigen::Matrix<double, StateType::DIM, StateType::DIM> StateCovarianceType;
+	typedef Eigen::Matrix<double, StateType::DIM, StateType::DIM> Matrix;
 	
 	
 	
@@ -96,7 +123,7 @@ class KalmanFilter {
 	ProcessModelType processModel;
 	
 	void predict(double dt) {
-		const StateCovarianceType A(processModel.getJacobian(state, dt));
+		const Matrix A(processModel.getJacobian(state, dt));
 		state.covariance = A * state.covariance * A.transpose() + processModel.getNoiseCovariance(dt);
 		/// @todo symmetrize?
 		processModel.updateState(state, dt);		
@@ -105,6 +132,35 @@ class KalmanFilter {
 	template<class MeasurementType>
 	void correct(MeasurementType & m) {
 		/// @todo implement
+		const Eigen::Matrix<double, MeasurementType::DIM, StateType::DIM> & H = m.getJacobian(state);
+		const Eigen::Matrix<double, MeasurementType::DIM, MeasurementType::DIM> & R = m.getCovariance(state);
+		const Eigen::Matrix<double, StateType::DIM, 1> innovation = m.getInnovation(state);
+		const Eigen::Matrix<double, MeasurementType::DIM, MeasurementType::DIM> S = H * state.covariance * H.transpose() + R;
+		
+		Eigen::LDLT<Eigen::Matrix<double, MeasurementType::DIM, MeasurementType::DIM> > ldltOfS = S.ldlt();
+		/*
+		bool result = ldltOfS.solve(state.covariance * H.transpose(), &K);
+		if (!result) {
+			throw new std::runtime_error("cholesky failed!");
+		}
+		Matrix covCorrection;
+		if (!ldltOfS.solve(state.covariance * H.transpose(), & covCorrection)) {
+			throw new std::runtime_error("First ldlt failed!");
+		}
+		state.covariance -= covCorrection;
+		
+		Matrix stateInnovPart;
+		if (!ldltOfS.solve(innovation, &stateInnovPart)) {
+			throw new std::runtime_error("Second ldlt failed!");
+		}
+		 processModel.updateFromMeasurement(state, state.covariance * H.transpose() * stateInnovPart);
+
+		 */
+		Eigen::LU<Matrix> luOfS = S.lu();
+		Matrix K = state.covariance * H.transpose() * luOfS.inverse();
+		processModel.updateFromMeasurement(state, K * innovation);
+		state.covariance = (Matrix::Identity() - K*H)*state.covariance;
+		
 	}
 	
 	
