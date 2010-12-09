685d11943ff220ba080e3bdcfc48fd23cf17c177
Ryan Pavlik <rpavlik@iastate.edu>
2010-12-09 06:45:00 -0600

    actually do something in the app

--- kftest.cpp
+++ kftest.cpp
@@ -22,6 +22,20 @@
 // - none
 
 int main(int argc, char * argv[]) {
+	typedef SimpleState<2> state_t;
+	typedef ConstantProcess<2, state_t> process_t;
+	KalmanFilter<state_t, process_t> kf;
+	double dt = 0.5;
+	for (double t = 0; t < 5.0; t+= dt) {
+		kf.predict(dt);
+		double noise = (std::rand() % 5) / 5.0;
+		
+		Eigen::Vector2d pos(Eigen::Vector2d::Constant(t));
+		AbsoluteMeasurement<state_t> meas;
+		meas.x = (pos + Eigen::Vector2d::Constant(noise)).eval();
+		kf.correct(meas);
+		std::cout << kf.state.vec << std::endl;
+	}
 	return 0;
 
 }
