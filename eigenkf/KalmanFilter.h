57212004f66789caf2ff694d1fc2be207112bd79
Ryan Pavlik <rpavlik@iastate.edu>
2010-12-09 11:10:13 -0600

    minor cleanups

--- eigenkf/KalmanFilter.h
+++ eigenkf/KalmanFilter.h
@@ -28,14 +28,14 @@ namespace eigenkf {
 			static const int DIM = dimension;
 			typedef Eigen::Matrix<double, dimension, 1> VecState;
 			typedef Eigen::Matrix<double, dimension, dimension> MatStateState;
-	
+
 			VecState x;
 			MatStateState covariance;
-	
+
 			SimpleState() :
 				x(VecState::Zero()),
 				covariance(MatStateState::Identity()) { }
-	
+
 	};
 
 	template<int dimension, class StateType>
@@ -43,37 +43,37 @@ namespace eigenkf {
 		public:
 			typedef Eigen::Matrix<double, StateType::DIM, 1> VecState;
 			typedef Eigen::Matrix<double, StateType::DIM, StateType::DIM> MatStateState;
-	
-	
+
+
 			/// Noise per second, per component
 			VecState sigma;
 			MatStateState noise;
 			MatStateState jacobian;
-	
+
 			inline ConstantProcess() :
 				sigma(VecState::Zero()),
 				noise(MatStateState::Zero()),
-				jacobian(MatStateState::Identity()) 
+				jacobian(MatStateState::Identity())
 			{}
-	
+
 			MatStateState const& getJacobian(StateType const& /*state*/, const double /*dt*/) {
 				/// No change over time due to process model
 				return jacobian;
 			}
-	
+
 			void updateState(StateType const& /*state*/, const double /*dt*/) {
 				/// no-op - no change due to process model
 			}
-	
+
 			MatStateState const& getNoiseCovariance(const double dt) {
 				noise = (dt * sigma).asDiagonal();
 				return noise;
 			}
-		
+
 			void updateFromMeasurement(StateType & state, VecState const & innovation) {
 				state.x += innovation;
 			}
-	
+
 	};
 
 	template<class StateType>
@@ -81,34 +81,35 @@ namespace eigenkf {
 	public:
 		static const int DIM = StateType::DIM;
 		typedef Eigen::Matrix<double, DIM, 1> VecMeas;
+		typedef Eigen::Matrix<double, DIM, 1> VecState;
 		typedef Eigen::Matrix<double, DIM, DIM> MatMeasMeas;
 		typedef Eigen::Matrix<double, DIM, DIM> MatMeasState;
-		
+
 		VecMeas measurement;
-	
+
 		MatMeasState jacobian;
 		MatMeasMeas covariance;
-	
+
 		AbsoluteMeasurement() :
 			measurement(VecMeas::Zero()),
 			jacobian(MatMeasState::Identity()),
 			covariance(MatMeasMeas::Identity()) {}
-	
-	
+
+
 		MatMeasState const& getJacobian(StateType const& state) {
 			return jacobian;
 		}
-	
+
 		/// Measurement noise covariance, aka uncertainty
 		/// in measurement
 		MatMeasMeas const& getCovariance(StateType const& state) {
 			return covariance;
 		}
-	
-		VecMeas const getInnovation(StateType const& state) {
+
+		VecState const getInnovation(StateType const& state) {
 			return measurement - state.x;
 		}
-	
+
 	};
 
 	template<class StateType, class ProcessModelType>
@@ -116,19 +117,19 @@ namespace eigenkf {
 		public:
 		typedef Eigen::Matrix<double, StateType::DIM, StateType::DIM> MatStateState;
 		typedef Eigen::Matrix<double, StateType::DIM, 1> VecState;
-	
-	
-	
+
+
+
 		StateType state;
 		ProcessModelType processModel;
-	
+
 		void predict(double dt) {
 			const MatStateState A(processModel.getJacobian(state, dt));
 			state.covariance = A * state.covariance * A.transpose() + processModel.getNoiseCovariance(dt);
 			/// @todo symmetrize?
-			processModel.updateState(state, dt);		
+			processModel.updateState(state, dt);
 		}
-	
+
 		template<class MeasurementType>
 		void correct(MeasurementType & m) {
 			typedef Eigen::Matrix<double, MeasurementType::DIM, StateType::DIM> MatMeasState;
@@ -141,7 +142,7 @@ namespace eigenkf {
 			const MatMeasMeas S = H * state.covariance * H.transpose() + R;
 		/*
 			Eigen::LDLT<MatMeasMeas> ldltOfS = S.ldlt();
-			
+
 			bool result = ldltOfS.solve(state.covariance * H.transpose(), &K);
 			if (!result) {
 				throw new std::runtime_error("cholesky failed!");
@@ -151,7 +152,7 @@ namespace eigenkf {
 				throw new std::runtime_error("First ldlt failed!");
 			}
 			state.covariance -= covCorrection;
-		
+
 			Matrix stateInnovPart;
 			if (!ldltOfS.solve(innovation, &stateInnovPart)) {
 				throw new std::runtime_error("Second ldlt failed!");
@@ -163,10 +164,10 @@ namespace eigenkf {
 			MatStateState K = state.covariance * H.transpose() * luOfS.inverse();
 			processModel.updateFromMeasurement(state, K * innovation);
 			state.covariance = (MatStateState::Identity() - K*H)*state.covariance;
-		
+
 		}
-	
-	
+
+
 	};
 
 } // end of namespace eigenkf
